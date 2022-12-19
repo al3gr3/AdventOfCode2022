94a2d8aee88f85a2be39113a6f0df512c24acbb0
al3gr3 <noreply>
2022-12-19 22:29:40 +0200

    Pr19 setup

---
+++ Pr19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Pr15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if TEST
+            var lines = File.ReadAllLines("TextFile1.txt");
+#else
+            var lines = File.ReadAllLines("TextFile2.txt");
+#endif
+
+        }
+    }
+}
